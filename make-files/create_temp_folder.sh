e578c9447e0aea94362b37a41df028fc6973bf00
riera90 <diegorodriguezriera@gmail.com>
2018-07-30 18:09:26 +0200

    deleted almost all for 2.0

--- make-files/create_temp_folder.sh
+++ make-files/create_temp_folder.sh
@@ -1,13 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 #	script for the creation of the temporal fyles for the makefile
-for file in $*; do
-	#directory=$(echo $file | sed -rn 's/^(.*)$/\1/p')
-	directory=$file
-	if [[ -d $directory ]]; then
-		echo "[already created] " $directory
-	else
-		if [[ "" != $directory ]]; then
-			mkdir -p $directory
-		fi
-	fi
-done
