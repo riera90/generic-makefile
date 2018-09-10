c2b50dd72c0a66c1c4705137a6e1c78b0d26f42f
riera90 <diegorodriguezriera@gmail.com>
2018-09-10 15:50:37 +0200

    fixed #12 added binary arguments

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "Greeter.hpp"
 
-int main() {
+int main(int argc, const char** argv) {
 	Greeter greeter;
 	greeter.greet();
 	std::cout << "\n";
+	for (int argumentIndex = 1; argumentIndex < argc; argumentIndex++)
+	{
+		printf("argument %i is %s\n", argumentIndex, argv[argumentIndex] );
+	}
 	return 0;
 }
