085a222eb62e9964a2f9492d035436d63ef1f620
diego@alpha <diegorodriguezriera@gmail.com>
2018-12-23 16:30:52 +0100

    first functional version of v3 (not stable)

--- example/make-files/config.sh
+++ example/make-files/config.sh
@@ -45,10 +45,19 @@ debugVariables(){
 		echo -e "\t"$lib
 	done
 	
-	echo "FLAGS_DIN_PATHS:"
-	for flag in $FLAGS_DIN_PATHS
+	echo "FLAGS_DIN:"
+	printf "\t"
+	for flag in $FLAGS_DIN
 	do
-		echo -e "\t"$flag
+		echo $flag | tr "\n" " "
+	done
+	printf "\n"
+	
+	echo "TMP_FOLDERS:"
+	printf "\t"
+	for dir in $TMP_FOLDERS
+	do
+		echo $dir | tr "\n" " "
 	done
 	echo -e "\n"
 	
@@ -81,7 +90,7 @@ loadVariables(){
 
 	BIN_ARGUMENTS=$(cat ./Makefile | sed -rn 's#^BIN_ARGUMENTS.*:=\ *(.*)$#\1#p')
 
-	CXX=$(cat ./Makefile | sed -rn 's#^CXX.*:=\ *(.*)$#\1#p')
+	CXX=$(cat ./Makefile | sed -rn 's#^CXX\ *:=\ *(.*)$#\1#p')
 
 	CXXFLAGS=$(cat ./Makefile | sed -rn 's#^CXXFLAGS.*:=\ *(.*)$#\1#p')
 
@@ -102,8 +111,9 @@ processVariables(){
 	
 	GTESTS=$(find $GTEST_DIR -name "*.$SRC_CODE_EXT" -type f)
 	
+	# if there is an extra libs folder, find all in the folder
 	if [[ $EXTRA_LIB_DIR ]]; then
-		EXTRA_LIBS=$(find $EXTRA_LIB_DIR -type f)
+		EXTRA_LIBS=$(find $EXTRA_LIB_DIR -type f | tr "\n" " ")
 	else
 		EXTRA_LIBS=""
 	fi
@@ -120,6 +130,12 @@ processVariables(){
 		fi
 	done
 	CODE=$AUX
+	
+	# dinamic linking flags
+	FLAGS_DIN_PATHS=$(find $SRC_DIR -type d)
+	
+	# binary file 
+	BINARY=$OUTPUT_DIR/$BIN_NAME
 	
 	# if there are excluded files, then delete the ones matching the regex
 	if [[ $EXCLUDED_FILES ]]; then
@@ -166,13 +182,49 @@ processVariables(){
 			done
 			GTESTS=$AUX
 		done
+		
+		for excluded in $EXCLUDED_FILES; do
+			unset AUX
+			AUX=""
+			for flag in $FLAGS_DIN_PATHS; do
+				if [[ "$flag" =~ "$excluded" ]]; then
+					echo -e "excluded!!" $flag
+					continue
+				else
+					AUX+=$flag" "
+				fi
+			done
+			FLAGS_DIN_PATHS=$AUX
+		done
+		
 	fi
+	
 	
 	# extra variables configuration
 	
-	FLAGS_DIN_PATHS=$(find $SRC_DIR -type d)
+	# add -I to the dinamic flags directory
+	for flag in $FLAGS_DIN_PATHS; do
+		FLAGS_DIN+="-I "$flag" "
+	done
 	
+	
+	# set the project library name
 	LIBRARY=$TMP_DIR/libproject.a
+	
+	# if the gdb is set to yes, add the -g flag 
+	if [[ $ALLOW_FOR_GNU_DEBUGGER == "YES" ]]; then
+		CXXFLAGS+=" -g"
+	fi
+	
+	# objects
+	for code in $CODE; do
+		# fist we change the src for tmp and then the code extension to .o
+		OBJECTS+=$(echo -e $code | sed -re 's#^'$SRC_DIR'(.*)\.'$SRC_CODE_EXT'$#'$TMP_DIR'\1.o#' )" "
+	done
+	OBJECTS=$(echo $OBJECTS | tr "\n" " ")
+	
+	# tmp folders
+	TMP_FOLDERS=$TMP_DIR" "$(find $SRC_DIR/* -type d | sed -re 's#^'$SRC_DIR'(.*)#'$TMP_DIR'\1#')
 }
 
 
@@ -190,15 +242,120 @@ generateMakefile(){
 	# if execute after compilation is set, put the run at the start,
 	# the  build, if not, first build then run
 	if [[ $EXECUTE_AFTER_COMPILATION == "YES" ]]; then
-echo -e "
+		echo "
 .PHONY : run
 run : $OUTPUT_DIR/$BIN_NAME
-	time \"$OUTPUT_DIR/$BIN_NAME\" $BIN_ARGUMENTS
+	@echo \"\tExecuting binary.\n\"
+	@time \"$OUTPUT_DIR/$BIN_NAME\" $BIN_ARGUMENTS
 
-$OUTPUT_DIR/$BIN_NAME : banner $TMP_FOLDERS $MAIN $EXTRA_LIBS $LIBRARY
 
+$OUTPUT_DIR/$BIN_NAME : banner $OUTPUT_DIR $MAIN $EXTRA_LIBS $LIBRARY
+	@echo \"\tCompiling binary.\"
+	$CXX $MAIN $CXXFLAGS $FLAGS_DIN -o $BINARY $LIBRARY $EXTRA_LIBS
+" >> ./make-files/Makefile
+
+	else
+		echo "
+$OUTPUT_DIR/$BIN_NAME : banner $OUTPUT_DIR $MAIN $EXTRA_LIBS $LIBRARY
+	@echo \"\tCompiling binary.\"
+	$CXX $MAIN $CXXFLAGS $FLAGS_DIN -o $BINARY $LIBRARY $EXTRA_LIBS
+
+
+.PHONY : run
+run : $OUTPUT_DIR/$BIN_NAME
+	@echo \"\tExecuting binary.\n\"
+	@time \"$OUTPUT_DIR/$BIN_NAME\" $BIN_ARGUMENTS
 " >> ./make-files/Makefile
 	fi
+	
+	
+
+
+	
+	# library compilation
+	echo "
+$LIBRARY : $OBJECTS $TMP_DIR
+	@echo \"\tBuilding project library.\"
+	ar -rsc $LIBRARY $OBJECTS
+" >> ./make-files/Makefile
+
+
+
+
+
+	# object compilation
+	for code in $CODE; do
+		# get the corresponding object name and path for the code
+		object=$(echo -e $code | sed -re 's#^'$SRC_DIR'(.*)\.'$SRC_CODE_EXT'$#'$TMP_DIR'\1.o#' )
+		# tries to guess the header into file
+		file=$(echo -e $code | sed -re 's#^(.*)\.'$SRC_CODE_EXT'$#\1.'$SRC_HEADERS_EXT'#' )
+		# if the header exist it will be asigned to header
+		header=$(find $SRC_DIR -wholename "$file" -type f)
+		folder=$(echo $object | sed -re 's#^(.*)/[^/]*\.o$#\1#')
+		echo "
+$object : $code $header $folder
+	@echo \"\tPrecompiling object for $code.\"
+	$CXX -c $code $FLAGS_DIN -o $object;
+" >> ./make-files/Makefile
+	done
+	
+	
+	
+
+
+
+	# temporal folders
+	for folder in $TMP_FOLDERS; do
+		echo "
+$folder : 
+	@mkdir -p $folder
+" >> ./make-files/Makefile 
+	done
+	echo "
+$OUTPUT_DIR : 
+	@mkdir -p $OUTPUT_DIR 
+" >> ./make-files/Makefile 
+
+
+	
+	
+	
+	# banner
+	echo "
+.PHONY : banner
+banner :
+	@cat ./make-files/fancy_banner_short.txt
+" >> ./make-files/Makefile
+
+
+	
+	
+	# help
+	echo "
+.PHONY : help
+help:
+	@cat ./make-files/banner.txt
+	@cat ./make-files/help.txt
+" >> ./make-files/Makefile
+
+
+
+
+
+
+	# clean
+	echo "
+.PHONY : clean
+clean:
+	@echo \"\tRemoving temporal files\"
+	@rm -rf $TMP_DIR
+	@echo \"\tRemoving binary files\"
+	@rm $OUTPUT_DIR/*.out
+" >> ./make-files/Makefile
+	
+
+
+	
 	
 	
 	# the ducky
